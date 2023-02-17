e2dbb5b7429047dbd50e5bc0d4f5e01d504976d9
Sn1cke <113979281+Sn1cke@users.noreply.github.com>
2023-02-17 10:15:29 +0100

    First commit

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+
+const root: ReactDOM.Root = ReactDOM.createRoot(
+  document.querySelector("#root") as HTMLDivElement
+);
