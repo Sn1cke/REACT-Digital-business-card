c2e072e977464106ca46c03f2b489dbe1edafb6a
Sn1cke <113979281+Sn1cke@users.noreply.github.com>
2023-02-17 18:34:12 +0100

    Add change

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -4,7 +4,7 @@ import { Icon } from "@iconify/react";
 export default function Info() {
   return (
     <div className="info-container">
-      <img src="placeholder.png" alt="placeholder" />
+      <div className="about-img"></div>
       <h2>Niclas Broberg</h2>
       <h4 className="title">Frontend Developer</h4>
       <div className="buttons">
