5de4cf66d037f43df24eec1bd5393ba9021d3c6a
Sn1cke <113979281+Sn1cke@users.noreply.github.com>
2023-02-17 19:36:22 +0100

    Change interests

--- src/components/interests.tsx
+++ src/components/interests.tsx
@@ -5,8 +5,8 @@ export default function Interests() {
     <div className="interests-container">
       <h3>Interests</h3>
       <p>
-        Programmer. Reader. Travel entusiast. Boardgame nerd. Language learning.
-        Psychology. Weightlifter.
+        Programming. Reading. Weightlifting. Travel entusiast. Boardgame nerd.
+        Language learning. Psychology.
       </p>
     </div>
   );
